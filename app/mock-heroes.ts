d430ced13ee82df7e77cf133f31e4906030a1744
cbarbet <cedric.barbet@gmail.com>
2016-11-11 00:10:54 +0100

    Initial commit (from official tutorial)

---
+++ app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export var HEROES: Hero[] = [
+  {id: 1, name: 'Deadshot'},
+  {id: 2, name: 'Wonder Woman'},
+  {id: 3, name: 'Flash'},
+  {id: 4, name: 'Green Lantern'},
+  {id: 5, name: 'Zatanna'},
+  {id: 6, name: 'Batman'},
+  {id: 7, name: 'Créature du marais'},
+  {id: 8, name: 'Green Arrow'},
+  {id: 9, name: 'Sinestro'}
+];
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
